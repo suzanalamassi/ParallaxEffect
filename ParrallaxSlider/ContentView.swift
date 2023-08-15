417f97cffb6593cb0d6d9af0efed1c53fbe87600
Suzan Alamassi <Suzanamassy@gmail.com>
2023-08-15 10:24:53 +0300

    Initial Commit

---
+++ ParrallaxSlider/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ParrallaxSlider
+//
+//  Created by Suzan Amassy on 15/08/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ParrallaxSlider/ParrallaxSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParrallaxSliderApp.swift
+//  ParrallaxSlider
+//
+//  Created by Suzan Amassy on 15/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct ParrallaxSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
